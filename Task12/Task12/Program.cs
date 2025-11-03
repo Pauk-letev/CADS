d5319a648f5e2bbadee819af665eda5302527d89
Pauk-letev <pashkaLitvinenko@gmail.com>
2025-11-03 22:09:13 +0300

    Task №12: Stack

---
+++ Task12/Task12/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        MyStack<int> stack = new MyStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        Console.WriteLine(stack.Search(0));
+        Console.WriteLine(stack.Search(4));
+        Console.WriteLine(stack.Search(5));
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+    }
+}
