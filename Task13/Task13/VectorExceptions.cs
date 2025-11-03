d5cea49622203a5c7bc0358388c9b355e5f14c55
Pauk-letev <pashkaLitvinenko@gmail.com>
2025-11-04 02:11:46 +0300

    Task №13: Reverse Polish Notation

---
+++ Task13/Task13/VectorExceptions.cs
@@ -0,0 +1,30 @@
+﻿class VectorException : Exception
+{
+    public VectorException() :
+        base("Какая-то ошибка!")
+    { }
+}
+class VectorArgumentNullException : ArgumentNullException
+{
+    public VectorArgumentNullException() :
+        base("Передаваемый объект не может быть null!")
+    { }
+}
+class VectorArgumentException : ArgumentException
+{
+    public VectorArgumentException() :
+        base("Неверный тип объекта!")
+    { }
+}
+class VectorOutOfMemoryException : OutOfMemoryException
+{
+    public VectorOutOfMemoryException() :
+        base("Слишком большой вектор!")
+    { }
+}
+class VectorIndexOutOfRangeException : Exception
+{
+    public VectorIndexOutOfRangeException() :
+        base("Индекс улетел...")
+    { }
+}
