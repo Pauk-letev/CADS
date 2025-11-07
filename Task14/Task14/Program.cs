4efebdc20d964673f09ff84561863a2d1b75504b
Pauk-letev <pashkaLitvinenko@gmail.com>
2025-11-07 11:38:24 +0300

    Task №14: Array Deque

---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        MyArrayDeque<int> myArrayDeque = new MyArrayDeque<int>();
+        for(int i = 0; i < 10; ++i)
+            myArrayDeque.Add(i);
+        Console.WriteLine(myArrayDeque.IsEmpty());
+    }
+}
