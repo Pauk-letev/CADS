1f236032c6a25b119cffc9c84e11e8c6971607c6
Pauk-letev <pashkaLitvinenko@gmail.com>
2025-11-10 13:04:24 +0300

    Task №15: Reading to Array Deque:

---
+++ Task15/Task15/ArrayDequeExceptions.cs
@@ -0,0 +1,18 @@
+﻿class ArrayDequeException : Exception
+{
+    public ArrayDequeException() :
+        base("Какая-то ошибка!")
+    { }
+}
+class ArrayDequeArgumentNullException : ArgumentNullException
+{
+    public ArrayDequeArgumentNullException() :
+        base("Передаваемый объект не может быть null!")
+    { }
+}
+class ArrayDequeOutOfMemoryException : OutOfMemoryException
+{
+    public ArrayDequeOutOfMemoryException() :
+        base("Слишком большой дек!")
+    { }
+}
