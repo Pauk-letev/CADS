4e78f52305571400d6cd906b6135ac4d3790450b
Pauk-letev <pashkaLitvinenko@gmail.com>
2025-11-11 15:28:13 +0300

    Task №16: Linked List

---
+++ Task16/Task16/LinkedListExceptions.cs
@@ -0,0 +1,18 @@
+﻿class LinkedListException : Exception
+{
+    public LinkedListException() :
+        base("Какая-то ошибка!")
+    { }
+}
+class LinkedListArgumentNullException : ArgumentNullException
+{
+    public LinkedListArgumentNullException() :
+        base("Передаваемый объект не может быть null!")
+    { }
+}
+class LinkedListIndexOutOfRangeException : Exception
+{
+    public LinkedListIndexOutOfRangeException() :
+        base("Индекс улетел...")
+    { }
+}
---
+++ Task16/Task16/MyNode.cs
@@ -0,0 +1,11 @@
+﻿class MyNode<T>
+{
+    public T Value { get; set; }
+    public MyNode<T>? Next;
+    public MyNode<T>? Prev;
+    public MyNode(T value)
+    {
+        Value = value;
+        Next = null; Prev = null;
+    }
+}
